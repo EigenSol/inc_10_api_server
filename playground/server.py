486efb17c2480a81e2cdde37dc779fab659e9701
Umer Choudhary <dummy@email.com>
2025-06-12 13:06:52 -0700

    misc

---
+++ playground/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app  = Flask(__name__)
+@app.route('/')
+def index():
+    return {'success' : True}
+
+@app.route('/users')
+def user():
+    return{'Name': 'Ali'}
+
+
+app.run()
+
+
